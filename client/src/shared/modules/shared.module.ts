ecd8a38acc45dae300fae195496b6e0b3291ecce
Honekawa <manhvp1810@gmail.com>
2024-08-07 23:25:34 +0700

    edit thanh navbar

--- client/src/shared/modules/shared.module.ts
+++ client/src/shared/modules/shared.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, RouterModule, ReactiveFormsModule, FormsModule],
-  exports: [RouterModule, ReactiveFormsModule, FormsModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AsyncPipe,
+  ],
+  exports: [RouterModule, ReactiveFormsModule, FormsModule, AsyncPipe],
 })
 export class SharedModule {}
