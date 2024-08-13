3acfd9f4b38279c24d4e7fef9bd8344c4997c1e1
KhoiLe <khoi.lh01519@sinhvien.hoasen.edu.vn>
2024-08-13 17:01:29 +0700

    anh manh cuu e

--- client/src/app/pages/layout/sub-pages/book-info/book-info.component.ts
+++ client/src/app/pages/layout/sub-pages/book-info/book-info.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../../../../shared/modules/shared.module';
 import { MaterialModule } from '../../../../../shared/modules/material.module';
+import {NgStyle} from "@angular/common";
 
 @Component({
   selector: 'app-book-info',
   standalone: true,
-  imports: [SharedModule, MaterialModule],
+  imports: [SharedModule, MaterialModule, NgStyle],
   templateUrl: './book-info.component.html',
   styleUrl: './book-info.component.scss',
 })
 export class BookInfoComponent {
 
+  url=`../public/assets/harry_potter_va_hon_da_phu_thuy__j_k_rowling%203.png`;
+  style = 'background-image: url(' + this.url + ')';
 }
