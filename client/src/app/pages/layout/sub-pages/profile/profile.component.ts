f3b1cd30f560b5a5138150c9da107aa5d9013521
PhamLong <Phamhoanglong1910@gmail.com>
2024-08-15 12:07:54 +0700

    page Login-Profile mới sữa

--- client/src/app/pages/layout/sub-pages/profile/profile.component.ts
+++ client/src/app/pages/layout/sub-pages/profile/profile.component.ts
@@ -9,4 +9,12 @@ import { MaterialModule } from '../../../../../shared/modules/material.module';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      console.log('Selected file:', file.name);
+    }
+  }
+}
