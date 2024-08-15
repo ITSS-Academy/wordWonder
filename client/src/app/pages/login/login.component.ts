1428e85882be815ec0520eb90b26011ca1d7a1d9
Honekawa <manhvp1810@gmail.com>
2024-08-15 15:17:16 +0700

    complete login form

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
+import { MaterialModule } from '../../../shared/modules/material.module';
+import { SharedModule } from '../../../shared/modules/shared.module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatButton],
+  imports: [MaterialModule, SharedModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  isLoginWithEmailAndPass = false;
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Handle form submission
+      console.log(this.loginForm.value);
+    }
+  }
+
+  changeLoginMethod() {
+    this.isLoginWithEmailAndPass = !this.isLoginWithEmailAndPass;
+  }
+}
