d15e1a9060a5fb6c3cac96b6d85a575a2b5489ee
AnhLong <phamhoanglong1910@gmail.com>
2024-08-26 01:46:40 +0700

    nop bai api server

--- server/src/domains/user_ebooks/user_ebooks.controller.ts
+++ server/src/domains/user_ebooks/user_ebooks.controller.ts
@@ -1,34 +1,60 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { UserEbooksService } from './user_ebooks.service';
 import { CreateUserEbookDto } from './dto/create-user_ebook.dto';
 import { UpdateUserEbookDto } from './dto/update-user_ebook.dto';
+import { Public } from '../../utils/custom_decorators';
+import { User } from '../users/entities/user.entity';
 
 @Controller('user-ebooks')
 export class UserEbooksController {
   constructor(private readonly userEbooksService: UserEbooksService) {}
 
+  @Public()
   @Post()
-  create(@Body() createUserEbookDto: CreateUserEbookDto) {
-    return this.userEbooksService.create(createUserEbookDto);
+  async create(@Body() createUserEbookDto: CreateUserEbookDto) {
+    return await this.userEbooksService.create(createUserEbookDto);
   }
 
+  @Public()
   @Get()
-  findAll() {
-    return this.userEbooksService.findAll();
+  listUserHistory() {
+    return this.userEbooksService.listUserHistory();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userEbooksService.findOne(+id);
+  @Public()
+  @Get('one')
+  async findOneBy(
+    @Query('userId') userId: string,
+    @Query('ebookId') ebookId: string,
+  ) {
+    return await this.userEbooksService.findOneByEbookIdAndUserId(
+      ebookId,
+      userId,
+    );
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserEbookDto: UpdateUserEbookDto) {
-    return this.userEbooksService.update(+id, updateUserEbookDto);
+  @Public()
+  @Patch('')
+  update(
+    @Query('userId') userId: string,
+    @Query('ebookId') ebookId: string,
+    @Body() updateUserEbookDto: UpdateUserEbookDto,
+  ) {
+    return this.userEbooksService.update(userId, ebookId, updateUserEbookDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userEbooksService.remove(+id);
+  @Public()
+  @Delete(':user')
+  remove(@Param('user') user: User) {
+    return this.userEbooksService.remove(user);
   }
 }
