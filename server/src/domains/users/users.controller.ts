25d5f10869b28d588fa3381a0caf213c4695e7f6
DuyPhuc <pt74009@gmail.com>
2024-08-27 10:10:06 +0700

    change user controller

--- server/src/domains/users/users.controller.ts
+++ server/src/domains/users/users.controller.ts
@@ -6,11 +6,9 @@ import {
   Patch,
   Param,
   Delete,
-  ParseIntPipe,
   Request,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
