5e4568ef51a7eecadc7770d10a1b924424acb0a5
Honekawa <manhvp1810@gmail.com>
2024-08-27 16:22:57 +0700

    them parse pdf file

--- client/src/app/pages/layout/sub-pages/reading/reading.component.ts
+++ client/src/app/pages/layout/sub-pages/reading/reading.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
 import { SharedModule } from '../../../../../shared/modules/shared.module';
 import { MaterialModule } from '../../../../../shared/modules/material.module';
 import { MatDialog } from '@angular/material/dialog';
@@ -12,7 +17,7 @@ import { SettingsDialogComponent } from './components/settings-dialog/settings-d
   styleUrl: './reading.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ReadingComponent {
+export class ReadingComponent implements OnInit {
   readonly dialog = inject(MatDialog);
 
   openDialog() {
@@ -20,6 +25,40 @@ export class ReadingComponent {
 
     dialogRef.afterClosed().subscribe((result) => {
       console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  ngOnInit(): void {
+    const fileInput = document.getElementById('file-input') as HTMLInputElement;
+    const textOutput = document.getElementById('text-output') as HTMLElement;
+
+    fileInput.addEventListener('change', () => {
+      const file = fileInput.files?.[0];
+
+      if (file) {
+        const reader = new FileReader();
+        reader.readAsArrayBuffer(file);
+        reader.onload = async () => {
+          if (reader.result) {
+            const pdf = await (window as any).pdfjsLib.getDocument({
+              data: reader.result,
+            }).promise;
+            const maxPages = pdf.numPages;
+
+            let text = '';
+            for (let pageNum = 1; pageNum <= maxPages; pageNum++) {
+              const page = await pdf.getPage(pageNum);
+              const content = await page.getTextContent();
+              const pageText = content.items
+                .map((item: any) => item.str)
+                .join('\n');
+              text += pageText + '\n';
+            }
+
+            textOutput.textContent = text;
+          }
+        };
+      }
     });
   }
 }
